201e98d405e7b9b77dc248d4fa2974750a487078
aeon <aeon_flux@eclipso.ch>
2022-01-28 02:54:29 +0000

    Adding hashcode/equals to Count

--- rtpstalk/src/main/java/pinorobotics/rtpstalk/messages/submessages/elements/Count.java
+++ rtpstalk/src/main/java/pinorobotics/rtpstalk/messages/submessages/elements/Count.java
@@ -1,10 +1,36 @@
 package pinorobotics.rtpstalk.messages.submessages.elements;
 
 import id.xfunction.XJsonStringBuilder;
+import java.util.Objects;
 
 public class Count implements SubmessageElement {
 
     public int value;
+
+    public Count() {
+
+    }
+    
+    public Count(int value) {
+        this.value = value;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Count other = (Count) obj;
+        return value == other.value;
+    }
 
     @Override
     public String toString() {
